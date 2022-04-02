c1fddb2d5058cd151c754424c32cf54c08a0de6e
yze142 <3245336455@qq.com>
2022-04-02 17:03:45 +0800

    初始化工程

---
+++ lagou-edu-parent/lagou-edu-entity/src/main/java/com/xiaoyu/entity/User.java
@@ -0,0 +1,173 @@
+package com.xiaoyu.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 用户表(User)实体类
+ *
+ * @author makejava
+ * @since 2022-04-02 13:28:54
+ */
+public class User implements Serializable {
+    private static final long serialVersionUID = -49538079868272160L;
+    /**
+     * 用户id
+     */
+    private Integer id;
+    /**
+     * 用户昵称
+     */
+    private String name;
+    /**
+     * 用户头像地址
+     */
+    private String portrait;
+    /**
+     * 注册手机
+     */
+    private String phone;
+    /**
+     * 用户密码（可以为空，支持只用验证码注册、登录）
+     */
+    private String password;
+    /**
+     * 注册ip
+     */
+    private String regIp;
+    /**
+     * 是否有效用户
+     */
+    private String accountNonExpired;
+    /**
+     * 账号是否未过期
+     */
+    private String credentialsNonExpired;
+    /**
+     * 是否未锁定
+     */
+    private String accountNonLocked;
+    /**
+     * 用户状态：ENABLE能登录，DISABLE不能登录
+     */
+    private String status;
+    /**
+     * 是否删除
+     */
+    private String isDel;
+    /**
+     * 注册时间
+     */
+    private Date createTime;
+    /**
+     * 记录更新时间
+     */
+    private Date updateTime;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPortrait() {
+        return portrait;
+    }
+
+    public void setPortrait(String portrait) {
+        this.portrait = portrait;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRegIp() {
+        return regIp;
+    }
+
+    public void setRegIp(String regIp) {
+        this.regIp = regIp;
+    }
+
+    public String getAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    public void setAccountNonExpired(String accountNonExpired) {
+        this.accountNonExpired = accountNonExpired;
+    }
+
+    public String getCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    public void setCredentialsNonExpired(String credentialsNonExpired) {
+        this.credentialsNonExpired = credentialsNonExpired;
+    }
+
+    public String getAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    public void setAccountNonLocked(String accountNonLocked) {
+        this.accountNonLocked = accountNonLocked;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getIsDel() {
+        return isDel;
+    }
+
+    public void setIsDel(String isDel) {
+        this.isDel = isDel;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+}
+
